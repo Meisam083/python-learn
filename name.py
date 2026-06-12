8b60ca2d87504cfe2481bcd60650e03b8bd140b9
Meysam <maysamshamshiri83@gmail.com>
2026-06-12 17:13:38 +0330

    print full name

--- name.py
+++ name.py
@@ -1,5 +1,4 @@
-name = "Meisam"
-print(name.title())
-
-print(name.upper())
-print(name.lower())
+first_name = "meisam"
+last_name = "sham"
+full_name = first_name + " " + last_name
+print(full_name)
